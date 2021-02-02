d0715c6ced85af2089a6d074f69c495c3c6930bf
preetika40 <preetika.singh40@gmail.com>
2021-02-02 12:50:26 +0530

    updated scss and js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,7 +1,10 @@
 const scrollBtn = document.querySelector(".footer__arrow-up");
 const rootElement = document.documentElement;
+
+//Function to be called on click on button
 scrollBtn.addEventListener("click", () => {
   rootElement.scrollTo ({
+    //Scroll up to the top of the page
     top: 0,
     behavior: "smooth"
   })
